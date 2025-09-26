7a8ab8eb8b1dd6de00e13043ade3858d30f5f96b
Victor-sarti <Sartivictor24@gmail.com>
2025-09-26 15:01:36 -0300

    Migrations with Flyway

---
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,6 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+(1,'São Paulo - Brasil','Bruno','M','Sarti'),
+(3,'Porbandar - India','José','M','Lucas'),
+(5,'Tokyo - Japão','Gabriela','F','Wharrionk'),
+(7,'Mvezo - South Africa','Lais','F','Sugarava'),
+(8,'Smiljan - Croacia','Kaua','M','Sugarava');
