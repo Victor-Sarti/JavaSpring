aa658a5a4dd7b14ff14bcc29cf0f50963aaad9fc
Victor-sarti <Sartivictor24@gmail.com>
2025-09-29 15:52:05 -0300

    Swagger implemented

---
+++ src/main/java/br/com/sarti/JavaSpring/config/OpenApiConfig.java
@@ -0,0 +1,27 @@
+package br.com.sarti.JavaSpring.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    OpenAPI customOpenAPI(){
+        return new OpenAPI()
+        .info(new Info()
+                .title("RESET API's RESTful from 0 with Java, Spring Boot, Kubernetes and Docker")
+                .version("v1")
+                .description("RESET API's RESTful from 0 with Java, Spring Boot, Kubernetes and Docker")
+                .termsOfService("https://github.com/Victor-Sarti")
+                .license( new License()
+                        .name("Apache 2.0")
+                        .url("https://github.com/Victor-Sarti")
+                )
+        );
+    }
+}
