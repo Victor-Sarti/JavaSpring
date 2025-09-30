cd39e55ab1f553d4a027988ab60c53b799e43a1d
Victor-sarti <Sartivictor24@gmail.com>
2025-09-30 16:03:19 -0300

    download method implemented

--- src/main/java/br/com/sarti/JavaSpring/services/FileStorageService.java
+++ src/main/java/br/com/sarti/JavaSpring/services/FileStorageService.java
@@ -2,10 +2,13 @@ package br.com.sarti.JavaSpring.services;
 
 import br.com.sarti.JavaSpring.FileStorageConfig;
 import br.com.sarti.JavaSpring.controllers.docs.FileControllerDocs;
+import br.com.sarti.JavaSpring.exeception.FileNotFoundException;
 import br.com.sarti.JavaSpring.exeception.FileStorageException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
@@ -61,6 +64,22 @@ public class FileStorageService {
         } catch (Exception e) {
             logger.error("Could not store file " + fileName + ". Please try Again!");
             throw new FileStorageException("Could not store file " + fileName + ". Please try Again!", e);
+        }
+    }
+
+    public Resource loadFileAsResource(String fileName) {
+        try {
+            Path filePath = this.fileStorageLocation.resolve(fileName).normalize();
+            Resource resource = new UrlResource(filePath.toUri());
+            if (resource.exists()) {
+                return resource;
+            } else {
+                logger.error("File not found " + fileName);
+                throw new FileNotFoundException("File not found " + fileName);
+            }
+        } catch (Exception e) {
+            logger.error("File not found " + fileName);
+            throw new FileNotFoundException("File not found " + fileName, e);
         }
     }
 }
